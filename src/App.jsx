57ee4c213fa268bf6cdc35cf2c2cb540ba791101
Marshall-Bits <kz_@hotmail.es>
2024-05-03 17:51:40 +0200

    add supabase

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,30 @@ import Navbar from "./components/Navbar";
 import RecipesPage from "./pages/RecipesPage";
 import recipesArray from "./data/recipes.json";
 import FormPage from "./pages/FormPage";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://aiyzzjbjaeirqtbcheio.supabase.co",
+  import.meta.env.VITE_SUPABASE_KEY
+);
 
 function App() {
-  const [recipes, setRecipes] = useState(recipesArray);
+  const [recipes, setRecipes] = useState([]);
   const [originalRecipes] = useState(recipesArray);
+
+  const getData = async () => {
+    const { data, error } = await supabase.from("recipes").select();
+    if (error) {
+      console.log("Error fetching data:", error.message);
+      return;
+    }
+    console.log("Data fetched successfully:", data);
+    setRecipes(data);
+  };
 
   return (
     <>
+      <button onClick={getData}>Get Data</button>
       <Navbar setRecipes={setRecipes} recipes={originalRecipes} />
       <Routes>
         <Route
